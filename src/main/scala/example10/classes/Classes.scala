4fc42c6ad9e6ca8f26b33ea2843d7f6b04cc6edb
Pavel Pozdeev <pasharik@mail.ru>
2020-05-20 09:04:02 +0100

    Scala class and constructor example

---
+++ src/main/scala/example10/classes/Classes.scala
@@ -0,0 +1,21 @@
+package example10.classes
+
+class Calculator {
+  val color = "White"
+}
+
+class Calculator2(brand: String) {
+  val color: String =
+    if (brand == "HP") "Black"
+    else "White"
+
+  def add(m: Int, n: Int): Int = m + n
+}
+
+object Classes extends App {
+  val c = new Calculator
+  println(c.color)
+
+  val c2 = new Calculator2("HP")
+  println(c2.color + " " + c2.add(1, 2))
+}
